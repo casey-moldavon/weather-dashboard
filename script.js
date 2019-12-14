9ac513c9fec6943ef0030ba8ae38fc3194ac24c7
Casey Yang <cmoldavon@gmail.com>
2019-12-13 23:11:51 -0800

    Updating script.js

--- script.js
+++ script.js
@@ -44,6 +44,29 @@
         $("#day-5-date").text(month + "/" + "20" + "/" + year)
     };
 
+//     var dayForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + ",us" + "&appid=" + apiKey;
+//     $.ajax({
+//         url: dayForecast,
+//         method: "GET"
+//     }).then(function (response) {
+//         console.log(response)
+//         var currentDay = 1;
+//         for (var i = 0; i < response.list.length; i+=8) {
+//             var data = response.list[i];
+//             console.log(newDate)
+//             //#day1()dynamic h2
+//             var htmlPath = '#day' + currentDay + ' ';
+//             var newDate = data.dt_txt.split(" ").shift()
+//             $(htmlPath + 'h4').text(newDate)
+//             // $(htmlPath + "img").append(data.i.icon)
+//             // console.log(icon)
+//             //TODO: implement other code above ++
+//             $(htmlPath + "p:first-child").text("Humidity: " + convertKToF(+ data.main.temp))
+//             $(htmlPath + "p:last-child").text("Humidity: " + data.main.humidity + "%")
+//             currentDay++;
+//         }
+//     });
+// });
 
 
 
